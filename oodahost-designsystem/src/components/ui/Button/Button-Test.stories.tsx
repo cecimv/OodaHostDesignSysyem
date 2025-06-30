94339ecad05c299100a0e07f2ffa173a0d08304c
CristinaMedina <ceci.m.vqz@gmail.com>
2025-06-30 13:58:41 -0600

    Update 30 - 06 - 2025 (modificacion de prueba)

--- oodahost-designsystem/src/components/ui/Button/Button-Test.stories.tsx
+++ oodahost-designsystem/src/components/ui/Button/Button-Test.stories.tsx
@@ -1,26 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { ButtonLightmode } from '@/components/ui/Button/Button-Lightmode'; // importa tu componente Input
+import { ButtonLightmode } from '@/components/ui/Button/Button-Lightmode';
 
 const meta: Meta<typeof ButtonLightmode> = {
-    title: 'components/MCPTest/ButtonTest',
-    component: ButtonLightmode,
-    parameters: {
-      layout: 'centered',
-      docs: {
-        description: {
-          component:
-            'This is an example test.',
-        },
+  title: 'Components/MCPTest/ButtonTest',
+  component: ButtonLightmode,
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component: 'This is an example test.',
       },
     },
-  };
-  export default meta;
+  },
+};
+export default meta;
 
 type Story = StoryObj<typeof ButtonLightmode>;
 
-export const primaryTextOnlySimple: Story = (args: React.ComponentProps<typeof ButtonLightmode>) => <ButtonLightmode {...args} />;primaryTextOnlySimple.args = {
-    children: "Text",
-    variant: "primaryTextOnly",
-    size: "md",
-  };
+export const primaryTextOnlySimple: Story = {
+  args: {
+    children: 'Text',
+    variant: 'primaryTextOnly',
+    size: 'md',
+  },
+};
--- oodahost-designsystem/src/components/ui/Input/Input-Test.stories.tsx
+++ oodahost-designsystem/src/components/ui/Input/Input-Test.stories.tsx
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Input } from '@/components/ui/Input/Input'; // importa tu componente Input
+import { Input } from '@/components/ui/Input/Input';
 
 const meta: Meta<typeof Input> = {
-    title: 'components/MCPTest/InputTest',
-    component: Input,
-    parameters: {
-      layout: 'centered',
-      docs: {
-        description: {
-          component:
-            'This is an example test.',
-        },
+  title: 'Components/MCPTest/InputTest',
+  component: Input,
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component: 'This is an example test.',
       },
     },
-  };
-  export default meta;
+  },
+};
+export default meta;
 
 type Story = StoryObj<typeof Input>;
 
-export const TextSimple: Story = (args: React.ComponentProps<typeof Input>) => <Input {...args} />;
-TextSimple.args = {
-  id: 'input-name',
-  label: ' ',
-  type: 'text',
-  placeholder: 'Text',
+export const TextSimple: Story = {
+  args: {
+    id: 'input-text',
+    label: ' ',
+    type: 'text',
+    placeholder: 'Text',
+  },
 };
